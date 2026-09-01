2ebe055af187d6ae7c71391a0de9e79b90fe9378
elango2601 <elangopugal26@gmail.com>
2026-09-01 19:09:38 +0530

    chore: cleanup patches

--- patch_rules2.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const file = 'firestore.rules';
-let code = fs.readFileSync(file, 'utf8');
-
-code = code.replace(/\.toLowerCase\(\)/g, '.lower()');
-
-fs.writeFileSync(file, code);
-console.log('patched firestore rules to lower()');
